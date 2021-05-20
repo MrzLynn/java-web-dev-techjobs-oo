7e1d1c3e6a98d4aa4cf5c916fa2e5c3139309fdd
Your Name <you@example.com>
2021-05-20 16:42:57 -0500

    Started Step 4 of assignment 2

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,14 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Test;
+
+public class JobTest {
+    @Test
+    public void testSettingJobId (){
+
+    }
+    @Test
+    public void testJobConstructorSetsAllFields (){
+
+    }
+}
