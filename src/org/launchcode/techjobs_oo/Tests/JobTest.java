fee47cf99caa0c913bbdfdcca24e48a9b9ca3abe
Your Name <you@example.com>
2021-05-20 20:45:44 -0500

    Started step 5

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -5,30 +5,45 @@ import org.junit.Test;
 import org.launchcode.techjobs_oo.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class JobTest {
-    private Job job1,job2, job3;
+    private Job job4,job5,job6,job7,job8;
 
     @Before
     public void settingUpTest (){
-        job1 = new Job();
-        job2 = new Job();
-        job3 = new Job("Front End Web", new Employer("LaunchCode"), new Location("Saint Louis"), new PositionType("Entry Level"), new CoreCompetency("Persistence"));
+        job4 = new Job();
+        job5 = new Job();
+        job6 = new Job("Front End Web", new Employer("LaunchCode"), new Location("Saint Louis"), new PositionType("Entry Level"), new CoreCompetency("Persistence"));
+        job7 = new Job("Back End Web", new Employer("Visa"), new Location("Atlantis"), new PositionType("Intermediate"), new CoreCompetency("Novice"));
+        job8 = new Job("Back End Web", new Employer("Visa"), new Location("Atlantis"), new PositionType("Intermediate"), new CoreCompetency("Novice"));
     }
+
     @Test
     public void testSettingJobId (){
-        assertEquals(1, job2.getId() - job1.getId());
+        assertEquals(1, job5.getId() - job4.getId());
     }
     @Test
     public void testJobConstructorSetsAllFields (){
-        assertEquals("Front End Web", job3.getName());
-        assertEquals("LaunchCode", job3.getEmployer().getValue());
-        assertEquals("Saint Louis", job3.getLocation().getValue());
-        assertEquals("Entry Level", job3.getPositionType().getValue());
-        assertEquals("Persistence", job3.getCoreCompetency().getValue());
+        assertEquals("Front End Web", job6.getName());
+        assertEquals("LaunchCode", job6.getEmployer().getValue());
+        assertEquals("Saint Louis", job6.getLocation().getValue());
+        assertEquals("Entry Level", job6.getPositionType().getValue());
+        assertEquals("Persistence", job6.getCoreCompetency().getValue());
     }
     @Test
     public void testJobsForEquality(){
+        assertFalse(job4.equals(job5));
+    }
+    @Test
+    public String toStringWorks() {
+        String jobOutput = (" ID: \n" + "name" + " Name: \n" + "employer" + " Location: \n" + "location" + " Position Type: \n" + "position type" + " Core Competency: \n" + "core competency");
+        if (jobOutput.contains("")) {
+            System.out.println("Data vot available");
+        } else
+            return jobOutput;
+    }
 
     }
+
 }
